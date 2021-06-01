643937e022ece86a08e27dd586414d4da8c6c858
Vow4excellence <53167685+Vow4excellence@users.noreply.github.com>
2021-06-01 13:27:42 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -15,11 +15,7 @@ function setup() {
 	world = engine.world;
 	//Create the Bodies Here.
 	ground1 = new Ground(800,680);
-  box1 = new Box(515,550,20,280);
-  box2 = new Box(625,660,240,20);
-  box3 = new Box(735,550,20,280);
-  Dustbin1 = new Dustbin(625,550,260,300);
-	Paper1 = new Paper(200,450,70);
+  
 	Engine.run(engine);
   
 }
@@ -30,18 +26,13 @@ function draw() {
   rectMode(CENTER);
   background(0);
   ground1.display();
-  box1.display();
-  box2.display();
-  box3.display();
-  Dustbin1.display();
-  Paper1.display();
- console.log(Paper1.body.position.y);
+  
 }
 
 
 function keyPressed(){
     if (keyCode === UP_ARROW){
-        Matter.Body.applyForce(Paper1.body, Paper1.body.position, {x:500, y:-1215})
+       //
     }
 }
 
